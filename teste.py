d831a0dc940869b3b5664d50d26388466a7b0f44
Gabriel-Col <gabrielerenocolombo@gmail.com>
2025-10-02 22:15:49 -0300

    Fibonacci

--- teste.py
+++ teste.py
@@ -4,5 +4,9 @@ def fatorial(a:int,acc:int)->int:
     else:
         return fatorial(a * 1,acc - 1)
 
-def fibonacci():
-    pass
+def fibonacci(a:int, acc1:int, acc2:int) -> int:
+    if a <= 1:
+        return acc1
+    else:
+        return fibonacci(a-1, acc2, acc1+acc2)
+    
